30cb66ee1fc00a087f2d4eac4d2cc91eecbe94c8
Fernando Flores <ferflores@gmail.com>
2014-06-16 11:22:38 -0500

    saving current changes

--- math/Garden/Garden.js
+++ math/Garden/Garden.js
@@ -19,6 +19,8 @@ function Garden(){
 		_this.context = canvas.getContext("2d");
 		_this.cx = canvas.width / 2;
 		_this.cy = canvas.height / 2;
+		_this.context.webkitImageSmoothingEnabled = true;
+		_this.context.lineCap = "rounded";
 
 		require(["../Vectors/Vector","../Particles/Particle"], function(Vectors) {
 			_this.configure();
@@ -30,7 +32,6 @@ function Garden(){
 		_this.erase();
 		_this.drawFLoor();
 		_this.createSeed();
-		_this.drawSeeds();
 		_this.bindEvents();
 	}
 
@@ -73,7 +74,7 @@ function Garden(){
 						var plantX = _this.seeds[x].particle.position.getX();
 						var plantY = _this.seeds[x].particle.position.getY();
 						var scaleFactor = Math.random() * _this.canvas.height/1.3 + 100;
-						plant.create(plantX, plantY, scaleFactor, 270, 0);
+						plant.create(plantX, plantY, scaleFactor, Garden.initialLineWIdth, 270, 0);
 						_this.plants.push(plant);
 
 						_this.seedsBeingProcessed--;
@@ -87,9 +88,13 @@ function Garden(){
 	}
 
 	this.drawPLants = function(){
-		for(var x=0;x< plants.length;x++){
+		for(var x=0;x< _this.plants.length;x++){
+			var plant = _this.plants[x];
 
+			plant.draw(_this.context);
+			
 		}
+		_this.context.stroke();
 	}
 
 	this.wrapSeed = function(seed){
@@ -127,10 +132,13 @@ function Garden(){
 		_this.erase();
 		_this.drawFLoor();
 		_this.drawSeeds();
+		_this.drawPLants();
 	}
 }
 
 Garden.maxBranchDepth = 3;
+Garden.initialLineWIdth = 4;
+Garden.posNeg = [-1,1];
 
 function Seed(){
 	this.particle = null;
@@ -162,31 +170,76 @@ function Plant(){
 	this.height = 0;
 	this.direction = 270;
 	this.branches = [];
-	this.color = "rgba(0,0,0,1)";
+	this.color = "#1D5E2A";
 	this.depth = 0;
+	this.growingLength = 0;
+	this.currentY = 0;
+	this.width = 0;
 
-	this.create = function(x,y, scaleFactor, direction, depth){
+	this.create = function(x,y, scaleFactor, width, direction, depth){
 		this.x = x;
 		this.y = y;
 		this.depth = depth;
+		this.width = width;
 
-		this.height = Math.random()*scaleFactor + 50;
+		this.height = Math.random()*scaleFactor + 20;
 		this.direction = direction || 270;
 
 		if(depth < Garden.maxBranchDepth){
 			var numberOfBranches = Math.random()*4+1;
 			for(var x=0;x< numberOfBranches; x++){
 				var plant = new Plant();
-				var branchX = this.x;
 				var branchHeight = Math.random() *this.height + 10;
-				var branchY = Math.random()*plant.height + 10;
-				var dir = Math.random() * 315  + 225;
-				plant.create(branchX, branchY, this.scaleFactor / 2, dir, this.depth+1);
+				var dir = this.direction + (Math.random() * 45 * Garden.posNeg[Math.round(Math.random()*1)]);
+				var randomRatio = Math.random() * this.height;
+				var branchX = Math.cos(this.direction * Math.PI / 180) * (randomRatio) + this.x;
+				var branchY = Math.sin(this.direction * Math.PI / 180) * (randomRatio) + this.y;
+				plant.create(branchX, branchY, scaleFactor / 3, this.width -1 < 1 ? 1 : this.width - 1 ,dir, this.depth+1);
 				this.branches.push(plant);
 			}
 		}
 	}
+
+	this.draw = function(context){
+
+		context.strokeStyle = this.color;
+
+		if(this.growingLength < this.height){
+			this.growingLength++;
+		}
+
+		var yPoint = this.currentY = Math.sin(this.direction * Math.PI / 180) * this.growingLength + this.y;
+		var xPoint = Math.cos(this.direction * Math.PI / 180) * this.growingLength + this.x;
+
+		var linesX = [];
+		var linesX2 = [];
+		var linesY = [];
+
+		for(var x = 0; x < this.width/2 ; x++){
+			linesX.push(this.x-x);
+			linesX2.push(xPoint-x);
+			linesY.push(yPoint-x);
+		}
+
+		for(var x = 0; x < this.width/2 ; x++){
+			linesX.push(this.x+x);
+			linesX2.push(xPoint+x);
+			linesY.push(yPoint+x);
+		}
+
+		for(var x=0;x< linesX.length;x++){
+			context.moveTo(linesX[x], this.y);
+			context.lineTo(linesX2[x], linesY[x]);
+		}
+
+		for(var y = 0; y< this.branches.length;y++){
+			if(this.currentY<this.branches[y].y){
+				this.branches[y].draw(context);
+			}
+		}
+	}
 }
+
 
 window.onload = function(){
 	chaos.init("Garden");
