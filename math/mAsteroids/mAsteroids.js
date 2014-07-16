0252ae728d0059f413f2f912508cd5bb9896c493
Fernando Flores <ferflores@gmail.com>
2014-07-15 20:41:18 -0500

    generate random rock

--- math/mAsteroids/mAsteroids.js
+++ math/mAsteroids/mAsteroids.js
@@ -8,6 +8,8 @@ function mAsteroids(){
 	this.turnLeft = false;
 	this.turnRight = false;
 	this.accelerating = false;
+	this.bullets = [];
+	this.enemies = [];
 
 	this.run = function(canvas, canvasWidth, canvasHeight){
 		_this = this;
@@ -24,6 +26,9 @@ function mAsteroids(){
 		_this.ship = new Ship();
 		_this.ship.create(_this.cx, _this.cy, Vector.create(0,0));
 		_this.bindEvents();
+		var stone = new Stone();
+		stone.create(200,200,0,0);
+		_this.enemies.push(stone);
 		_this.main();
 	}
 
@@ -40,7 +45,7 @@ function mAsteroids(){
 		document.body.addEventListener("keydown", function(e){
 			switch(e.keyCode){
 				case 38: //up
-					_this.ship.acceleration.setLength(0.5);
+					_this.ship.acceleration.setLength(0.2);
 					_this.ship.acceleration.setAngle(_this.ship.angle);
 					_this.accelerating = true;
 					break;
@@ -66,6 +71,9 @@ function mAsteroids(){
 				case 39: //right
 					_this.turnRight = false;
 					break;
+				case 32:
+					_this.fireBullet();
+					break;
 				default: 
 					break;
 			}	
@@ -77,6 +85,33 @@ function mAsteroids(){
 		_this.context.fillStyle = "#000000";
 		_this.context.fillRect(0, 0, _this.canvas.width, _this.canvas.height);
 		
+	}
+
+	this.fireBullet = function(){
+		var newBullet = new Bullet();
+		newBullet.create(_this.ship.frontPoint.x, _this.ship.frontPoint.y, _this.ship.angle);
+		_this.bullets.push(newBullet);
+	}
+
+	this.drawBullets = function(){
+		for(var x=0;x<_this.bullets.length;x++){
+			if(_this.bullets[x].particle.position.getX()>_this.canvas.width
+				|| _this.bullets[x].particle.position.getX()<0
+				|| _this.bullets[x].particle.position.getY()>_this.canvas.height
+				|| _this.bullets[x].particle.position.getY()<0){
+
+				_this.bullets.splice(x,1);
+			}else{
+				_this.bullets[x].draw(_this.context);
+				_this.bullets[x].particle.update();
+			}
+		}
+	}
+
+	this.drawEnemies = function(){
+		for(var x=0;x<_this.enemies.length;x++){
+			_this.enemies[x].draw(_this.context);
+		}
 	}
 
 	this.main = function(){
@@ -104,6 +139,9 @@ function mAsteroids(){
 		if(_this.ship.particle.position.getY() > _this.canvas.height)
 			_this.ship.particle.position.setY(0);
 
+		_this.drawBullets();
+		_this.drawEnemies();
+
 		requestAnimationFrame(_this.main);
 	}
 
@@ -114,6 +152,7 @@ function mAsteroids(){
 		this.angle = 0;
 		this.size = 10;
 		this.fireDrew = false;
+		this.frontPoint = null;
 
 		this.create = function(x, y, accel){
 			this.particle = Particle.create(x,y, 0, 0);
@@ -138,6 +177,8 @@ function mAsteroids(){
 				x: Math.cos(this.angle) * this.size + this.particle.position.getX(),
 				y: Math.sin(this.angle) * this.size + this.particle.position.getY()
 			}
+
+			this.frontPoint = dirPoint;
 
 			var nextPoint = {
 				x: Math.cos(this.angle+(Math.PI * 2 / 2.5)) * (this.size +10) + this.particle.position.getX(),
@@ -169,6 +210,57 @@ function mAsteroids(){
 		}
 	}
 
+	function Bullet(){
+		this.particle = null;
+		this.speed = 13;
+
+		this.create = function(x,y, dir){
+			this.particle = Particle.create(x,y,this.speed, dir);
+		}
+
+		this.draw = function(context){
+			var x = this.particle.position.getX();
+			var y = this.particle.position.getY();
+			context.fillStyle = "#FFFFFF";
+			context.beginPath();
+			context.moveTo(x,y);
+			context.arc(x, y, 2, 0, 2 * Math.PI, false);
+			context.fill();
+		}
+	}
+
+	function Stone(){
+		this.particle = null;
+		this.speed = 3;
+		this.points = [];
+
+		this.create = function(x,y,dir,speed){
+			this.particle = Particle.create(x,y,speed,dir);
+			var nPoints = Math.round(Math.random()*6+3);
+
+			var angleIncrement = (Math.PI*2)/nPoints;
+			var angleFactor = Math.PI;
+			for(var i = 0; i < nPoints ; i++){
+				var randomRadious = Math.random()*25+10;
+				var randomAngle = Math.random()*((angleFactor+angleIncrement)-angleFactor)+(angleFactor);
+				this.points.push({
+					x: Math.cos(randomAngle) * randomRadious + x,
+					y: Math.sin(randomAngle) * randomRadious + y
+				});
+				angleFactor+=angleIncrement;
+			}
+		}
+
+		this.draw = function(context){
+			context.strokeStyle = "#FFFFFF";
+			context.moveTo(this.points[0].x, this.points[0].y);
+			for(var x=1; x<this.points.length;x++){
+				context.lineTo(this.points[x].x, this.points[x].y);
+			}
+			context.lineTo(this.points[0].x, this.points[0].y);
+			context.stroke();
+		}
+	}
 
 }
 
