093d0ac75e4827da6ec64cdbaf21f3a6be909618
Fernando Flores <ferflores@gmail.com>
2014-08-16 11:22:21 -0500

    spiders

--- math/Spider/Spider.js
+++ math/Spider/Spider.js
@@ -7,6 +7,8 @@ function Spider(){
 	this.canvas = null;
 	this.spiders = [];
 	this.floorY = 0;
+	this.mouseX = 0;
+	this.mouseY = 0;
 
 	this.run = function(canvas, canvasWidth, canvasHeight){
 		_this = this;
@@ -24,15 +26,65 @@ function Spider(){
 	this.configure = function(){
 		_this.floorY = _this.canvas.height-30;
 		_this.erase();
-		var arania = new Arania();
-		arania.create(300,100,_this.canvas.height/20, _this.floorY);
-		_this.spiders.push(arania);
+		_this.bindEvents();
+		_this.createSpider(Math.random()*(_this.canvas.width-100)+100,Math.random()*100+100);
+	}
 
+	this.createSpider = function(x,y){
+		var arania = new Arania();
+		var randomWalkProb = Math.random()+0.5;
+		if(randomWalkProb > .90){
+			randomWalkProb = .90;
+		}
+		arania.create(x,y,_this.canvas.height/(Math.random()*40+20)	, _this.floorY, randomWalkProb);
+		_this.spiders.push(arania);
+	}
+
+	this.bindEvents = function(){
+		document.addEventListener("mousedown", function(e){
+			if(_this.spiders.length<20){
+				var mX = (e.pageX  - _this.canvas.offsetParent.offsetLeft);
+				var mY = (e.pageY  - _this.canvas.offsetParent.offsetTop);
+
+				_this.createSpider(mX, mY);
+			}
+		});
+
+		document.addEventListener("mousemove", function(e){
+			var mX = (e.pageX  - _this.canvas.offsetParent.offsetLeft);
+			var mY = (e.pageY  - _this.canvas.offsetParent.offsetTop);
+
+			for (var i = 0; i < _this.spiders.length; i++) {
+				var spider = _this.spiders[i];
+
+				var dy = mY - spider.headParticle.y;
+				var dx = mX - spider.headParticle.x;
+
+				var distance = Math.sqrt(dy*dy + dx*dx);
+
+				if(distance<50){
+					spider.walkProb = .0;
+					if(mX >= spider.headParticle.x){
+						spider.direction = 3.8;
+						
+					}else{
+						spider.direction = 5.5;
+					}
+				}else{
+					spider.direction = spider.defaultDirection;
+					spider.walkProb = spider.defaultWalkProb;
+				}
+			};
+		});
 	}
 
 	this.drawSpiders = function(){
 		for (var i = 0; i < _this.spiders.length; i++) {
 			_this.spiders[i].draw(_this.context);
+			var random = Math.random();
+			if(random>_this.spiders[i].walkProb){
+				_this.spiders[i].walk();
+			}
 		};
 	}
 
@@ -83,11 +135,18 @@ function Spider(){
 		this.radious = 0;
 		this.floorY = 0;
 		this.legLength = 25;
+		this.drag = false;
+		this.direction = 5.5;
+		this.walkProb = .8;
+		this.defaultWalkProb = .8;
+		this.defaultDirection = 5.5;
 		
-		this.create = function(x, y, size, floor){
+		this.create = function(x, y, size, floor, walkProb){
 			this.headParticle = Particle2.create(x,y,0,0,0.4);
 			this.size = size;
 			this.floorY = floor;
+			this.walkProb = walkProb;
+			this.defaultWalkProb = walkProb;
 
 			this.radious = this.size/4;
 
@@ -95,6 +154,7 @@ function Spider(){
 		}
 
 		this.createLegs = function(){
+			var k = 0.02;
 
 			var leg1Particle1 = Particle2.create(this.headParticle.x + this.legLength, this.headParticle.y - this.size/2,0,0);
 			var leg1Particle2 = Particle2.create(this.headParticle.x + this.legLength * 2, this.headParticle.y + this.size/2,0,0, 0.1);
@@ -103,12 +163,13 @@ function Spider(){
 				length:this.legLength
 				}
 
-			var k = 0.01;
+			
 
 			this.headParticle.addSpring(leg1Particle1, k,this.legLength);
 			leg1Particle1.addSpring(this.headParticle, k, this.legLength+5);
 			leg1Particle1.addSpring(leg1Particle2,k, this.legLength);
 			leg1Particle1.addSpring(invisible1.particle,k, this.legLength);
+			leg1Particle2.addSpring(leg1Particle1,k, this.legLength);
 			
 			var leg2Particle1 = Particle2.create(this.headParticle.x - this.legLength, this.headParticle.y - this.size/2,0,0);
 			var leg2Particle2 = Particle2.create(this.headParticle.x - (this.legLength*2), this.headParticle.y + this.size/2,0,0, 0.1);
@@ -121,35 +182,40 @@ function Spider(){
 			leg2Particle1.addSpring(this.headParticle, k, this.legLength+5);
 			leg2Particle1.addSpring(leg2Particle2, k, this.legLength);
 			leg2Particle1.addSpring(invisible2.particle, k, this.legLength);
+			leg2Particle2.addSpring(leg2Particle1,k, this.legLength);
 
-			var leg3Particle1 = Particle2.create(this.headParticle.x + (this.legLength*1.3), this.headParticle.y - this.size/2,0,0);
-			var leg3Particle2 = Particle2.create(this.headParticle.x + (this.legLength *2.4) , this.headParticle.y + this.size/2,0,0, 0.1);
+			var leg3Particle1 = Particle2.create(this.headParticle.x + (this.legLength*2), this.headParticle.y - this.size/2,0,0);
+			var leg3Particle2 = Particle2.create(this.headParticle.x + (this.legLength * 4), this.headParticle.y + this.size/2,0,0, 0.1);
 			var invisible3 = {
-				particle: Particle2.create(this.headParticle.x + this.legLength, this.headParticle.y+this.radious,0,0,0),
-				length:(this.legLength*1.3)
+				particle: Particle2.create(this.headParticle.x + (this.legLength*2), this.headParticle.y+this.radious,0,0,0),
+				length:this.legLength
 				}
 
-			/*this.headParticle.addSpring(leg1Particle1, k,this.legLength);
+			
+
+			this.headParticle.addSpring(leg3Particle1, k,this.legLength);
 			leg3Particle1.addSpring(this.headParticle, k, this.legLength+5);
 			leg3Particle1.addSpring(leg3Particle2,k, this.legLength);
-			leg3Particle1.addSpring(invisible3.particle,k, this.legLength);
+			leg3Particle1.addSpring(invisible1.particle,k, this.legLength);
+			leg3Particle2.addSpring(leg3Particle1,k, this.legLength);
 			
-			var leg4Particle1 = Particle2.create(this.headParticle.x - (this.legLength * 1.3), this.headParticle.y - this.size/2,0,0);
-			var leg4Particle2 = Particle2.create(this.headParticle.x - (this.legLength*2.4), this.headParticle.y + this.size/2,0,0, 0.1);
+			var leg4Particle1 = Particle2.create(this.headParticle.x - (this.legLength*2), this.headParticle.y - this.size/2,0,0);
+			var leg4Particle2 = Particle2.create(this.headParticle.x - (this.legLength*4), this.headParticle.y + this.size/2,0,0, 0.1);
 			var invisible4 = {
-				particle: Particle2.create(this.headParticle.x - this.legLength, this.headParticle.y+this.radious,0,0,0),
-				length:-this.legLength*2.4
+				particle: Particle2.create(this.headParticle.x - (this.legLength*2), this.headParticle.y+this.radious,0,0,0),
+				length:-this.legLength
 				}
 
 			this.headParticle.addSpring(leg4Particle1, k,this.legLength);
 			leg4Particle1.addSpring(this.headParticle, k, this.legLength+5);
 			leg4Particle1.addSpring(leg4Particle2, k, this.legLength);
-			leg4Particle1.addSpring(invisible4.particle, k, this.legLength);*/
+			leg4Particle1.addSpring(invisible2.particle, k, this.legLength);
+			leg4Particle2.addSpring(leg4Particle1,k, this.legLength);
 
-			this.legs.push({ p1:leg1Particle1, p2:leg1Particle2, invisible: invisible1});
-			this.legs.push({ p1:leg2Particle1, p2:leg2Particle2, invisible: invisible2});
-			//this.legs.push({ p1:leg3Particle1, p2:leg3Particle2, invisible: invisible3});
-			//this.legs.push({ p1:leg4Particle1, p2:leg4Particle2, invisible: invisible4});
+			this.legs.push({ p1:leg1Particle1, p2:leg1Particle2, invisible: invisible1, moving:false});
+			this.legs.push({ p1:leg2Particle1, p2:leg2Particle2, invisible: invisible2, moving:false});
+			this.legs.push({ p1:leg3Particle1, p2:leg3Particle2, invisible: invisible3, moving:false});
+			this.legs.push({ p1:leg4Particle1, p2:leg4Particle2, invisible: invisible4, moving:false});
 			
 		}
 
@@ -161,13 +227,41 @@ function Spider(){
 
 			if(this.headParticle.y + this.radious > this.floorY){
 				this.headParticle.y = this.floorY - this.radious;
-				this.headParticle.setSpeed(this.headParticle.getSpeed()*this.headParticle.bounce*0.6);
+				this.headParticle.setSpeed(this.headParticle.getSpeed()*this.headParticle.bounce*0.3);
+			}
+
+			if(this.headParticle.x > _this.canvas.width){
+				this.direction = 3.8;
+				this.defaultDirection = 3.8;
+			}
+
+			if(this.headParticle.x < 0){
+				this.direction = 5.5;
+				this.defaultDirection = 5.5;
+			}
+
+		}
+
+		this.walk = function(){
+			if(this.headParticle.y > this.floorY - 30){
+				var randomLeg = parseInt(Math.random()*(this.legs.length-1)+1);
+
+				if(!this.legs[randomLeg].moving){
+					this.legs[randomLeg].moving = true;
+					this.legs[randomLeg].p1.y-=3;
+					this.legs[randomLeg].p1.setSpeed(2);
+					this.legs[randomLeg].p1.setHeading(this.direction);
+				}
 			}
 		}
 
 		this.drawHead = function(c){
 			c.beginPath();
 			c.fillStyle = "#FFFFFF";
+			if(this.drag){
+				this.headParticle.x = _this.mouseX;
+				this.headParticle.y = _this.mouseY;
+			}
 			c.moveTo(this.headParticle.x, this.headParticle.y);
 			c.arc(this.headParticle.x, this.headParticle.y,this.radious,0,Math.PI*2,false);
 			c.fill();
@@ -193,6 +287,7 @@ function Spider(){
 				if(p2.y > this.floorY){
 					p2.y = this.floorY;
 					p2.setSpeed(p2.getSpeed()*p2.bounce*0.2);
+					this.legs[i].moving = false;
 				}
 
 				p1.update();
